d1da203aa5b6c068062fc17e1bbfc278db9a0793
Gulzar <gulzarhuseynovaa@gmail.com>
2024-12-11 01:45:43 +0400

    first commit

---
+++ notary.js
@@ -0,0 +1,28 @@
+
+
+document.querySelector(".legal2 .btn1").addEventListener("click",function(){
+    
+    if(document.querySelector(".legal2 p").style.display="none"){
+        
+        document.querySelector(".legal2 p").style.display="block"
+    }
+    
+    
+})
+
+document.querySelector(".legal2 .btn2").addEventListener("click",function(){
+    if(document.querySelector(".p2").style.display="none"){
+        
+        document.querySelector(".p2").style.display="block"
+    }
+    
+    
+})
+document.querySelector(".legal2 .btn3").addEventListener("click",function(){
+    if(document.querySelector(".p3").style.display="none"){
+        
+        document.querySelector(".p3").style.display="block"
+    }
+    
+    
+})
